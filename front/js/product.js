552a4d3efaa822245d461f81c2ccf1f69b18f2d9
camric <ymontantlointain@gmail.com>
2022-02-08 20:22:41 +0100

    product.js

--- front/js/product.js
+++ front/js/product.js
@@ -1,3 +1,4 @@
+
 // recuperer url
 var str_Url = document.location.href;
 var url = new URL(str_Url);
@@ -5,19 +6,17 @@ var url = new URL(str_Url);
 // recuperer id
 var productId = url.searchParams.get("id");
 
-// recuperer product list
+// recuperer produit
+
 loadConfig().then(data => {
     config = data;
-    fetch(config.host + "/api/products").then(data => data.json())
-        .then(jsonListProduct => {
-            for (let jsonProduct of jsonListProduct) {
-                let product = new Product(jsonProduct);
-                if (product._id == productId) {
-                    document.querySelector(".item__img").innerHTML += `<img src="${product.imageUrl}" alt="${product.altTxt00}" />`;
-                    document.getElementById("title").textContent = product.name;
-                    document.getElementById("price").textContent = product.price;
-                    document.getElementById("description").textContent = product.description;
-                }
-            }
+    fetch(config.host + "/api/products/" + productId)
+        .then(data => data.json())
+        .then(jsonProduct => {
+            let product = new Product(jsonProduct);
+            document.querySelector(".item__img").innerHTML += `<img src="${product.imageUrl}" alt="${product.altTxt00}" />`;
+            document.getElementById("title").textContent = product.name;
+            document.getElementById("price").textContent = product.price;
+            document.getElementById("description").textContent = product.description;
         })
 })
